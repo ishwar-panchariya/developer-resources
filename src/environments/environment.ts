45574689919196b2030bf330304c0dd04dc22c58
ishwar-panchariya <ishwarpanchariya17@gmail.com>
2025-12-23 18:48:11 +0530

    env file

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig: {
+    apiKey: "AIzaSyB9ouvTa0CxnOblwxGdSqXItNA6d86zhkU",
+    authDomain: "developer-resources-2c411.firebaseapp.com",
+    projectId: "developer-resources-2c411",
+    storageBucket: "developer-resources-2c411.firebasestorage.app",
+    messagingSenderId: "27205182354",
+    appId: "1:27205182354:web:9e8e51058a074782cd574b",
+    measurementId: "G-VNBVEKF562"
+    }
+};
